b91063146d1309b013e6347f861295c0515c918b
Mike Cruz <mikeedriccruz12@gmail.com>
2019-09-04 10:04:30 +0800

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function navToggle() {
+  var navLinks = document.getElementById("nav-links");
+  if (navLinks.style.display === "block") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "block";
+  }
+}
